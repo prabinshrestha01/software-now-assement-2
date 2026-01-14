1a8d9a0398cd08236f9a524221fea7da73973fc8
Nabin Thapa <nt12thapa@gmail.com>
2026-01-14 13:25:32 +0930

    Update decrypt_program.py

--- decrypt_program.py
+++ decrypt_program.py
@@ -1,18 +1,18 @@
 from main import decrypt_text  # improt decrypt function
 
-# Read encrypted text
+# Reading encrypted text
 with open("encrypted_text.txt", "r", encoding="utf-8") as f:
     encrypted = f.read()
 
-# Read shifts
+# Reading shifts
 with open("shifts.txt", "r", encoding="utf-8") as f:
-    shifts = list(map(int, f.read().split()))
+    shift = list(map(int, f.read().split()))
 
-# Decrypt using imported function
+# descrypt with imported  function
 decrypted = decrypt_text(encrypted, shifts)
 
-# Save decrypted text
+# save text from decrypted
 with open("decrypted_text.txt", "w", encoding="utf-8") as f:
     f.write(decrypted)
 
-print("Decryption done ✔")
+print("description done ✔")
